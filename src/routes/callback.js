3c49608d0370072b0504f0defa91a1efe74b649f
Michael Rommel <rommel@layer-7.net>
2022-01-12 20:16:35 +0100

    Add OAuth login

---
+++ src/routes/callback.js
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch'
+const tokenURL = 'https://github.com/login/oauth/access_token'
+const userURL = 'https://api.github.com/user'
+
+export async function get(request) {
+  const code = request.query.get('code')
+  const token = await getToken(code)
+  const user = await getUser(token)
+
+  request.locals.user = user.login
+
+  return {
+    status: 302,
+    headers: {
+      location: '/'
+    }
+  }
+}
+
+function getUser(token) {
+  return fetch(userURL, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  }).then(reply => reply.json())
+}
+
+function getToken(code) {
+  return fetch(tokenURL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      client_id: import.meta.env.VITE_CLIENT_ID,
+      client_secret: import.meta.env.VITE_CLIENT_SECRET,
+      code
+    })
+  }).then(reply => reply.json())
+    .then(reply => reply.access_token)
+}
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,24 @@
-<h1>Welcome</h1>
+<script context="module">
+  export async function load({ session }) {
+    return {
+      props: {
+        user: session.user
+      }
+    };
+  }
+</script>
+
+<script>
+  export let user;
+</script>
+
+<h1>Welcome {user}</h1>
+{#if user}
+  <a href="/logout">
+    <button>Logout</button>
+  </a>
+{:else}
+  <a href="/login">
+    <button>Github Login</button>
+  </a>
+{/if}
