0f2ba239ddba388e773ee576fbf224f70b9ba511
Michael Rommel <rommel@layer-7.net>
2022-01-13 14:10:45 +0100

    Add tailwind base config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,16 +9,22 @@
 </script>
 
 <script>
+  import '../app.css';
   export let user;
 </script>
 
-<h1>Welcome {user}</h1>
-{#if user}
-  <a rel="external" href="/logout">
-    <button>Logout</button>
-  </a>
-{:else}
-  <a rel="external" href="/login">
-    <button>Github Login</button>
-  </a>
-{/if}
+<div class="w-screen p-2">
+  <h1>Welcome {user}</h1>
+
+  <div>
+    {#if user}
+      <a rel="external" href="/logout">
+        <button class="bg-slate-200">Logout</button>
+      </a>
+    {:else}
+      <a rel="external" href="/login">
+        <button class="bg-slate-200">Github Login</button>
+      </a>
+    {/if}
+  </div>
+</div>
