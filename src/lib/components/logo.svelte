f734395c266e0bb014e8c9c79317d7cfcf9879d4
Michael Rommel <rommel@layer-7.net>
2022-01-14 15:50:45 +0100

    Add logo as component with color settings

---
+++ src/lib/components/logo.svelte
@@ -0,0 +1,13 @@
+<script>
+  import BigLogo from './miro-logo-2020-15.svg';
+  import SmallLogo from './miro-logo-2020-15-reduced-wo-wings.svg';
+  export let small;
+</script>
+
+{#if small}
+  <!-- <SmallLogo viewBox="0 0 510 510" class="logo dark:logodark" /> -->
+  <SmallLogo class="logo dark:logodark" />
+{:else}
+  <!-- <BigLogo viewBox="0 0 1204 504" class="logo dark:logodark" /> -->
+  <BigLogo class="logo dark:logodark" />
+{/if}
