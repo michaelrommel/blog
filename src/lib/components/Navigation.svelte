0be53c075dfaffdf739925cba837a2ff32a5cba1
Michael Rommel <rommel@layer-7.net>
2022-01-25 17:58:25 +0100

    Add test for column based notes or images

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -33,12 +33,12 @@
   }
 </script>
 
-<header>
-<nav class='mx-2 md:mx-4 lg:mx-8'>
+<header class="sticky top-0 z-10 bg-gruvdbg/90 backdrop-blur-sm">
+  <nav class='mx-2 md:mx-4 lg:mx-8'>
     <div class="w-full flex items-center justify-between">
       <div class='grow py-2 xs:py-4 flex items-center justify-start'>
         <div class='w-8 xs:w-16'>
-          <Logo small class='w-1' />
+          <Logo small class='' />
         </div>
         <div class='text-gruvlfg dark:text-gruvdfg flex items-center'>
           <div class='mx-4 md:mx-8 lg:mx-12 space-x-2 xs:space-x-4 md:space-x-8'>
