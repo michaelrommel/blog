08976b3396768cfd297061fac17577a439b2f471
Michael Rommel <rommel@layer-7.net>
2022-01-25 22:37:33 +0100

    Add markdown directive support

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,18 +1,54 @@
+import remarkGithub from 'remark-github';
+import remarkEmoji from 'remark-emoji';
+import remarkParse from 'remark-parse';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkDirective from 'remark-directive';
+import remarkGfm from 'remark-gfm';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { visit } from 'unist-util-visit';
+import { h } from 'hastscript';
+
+// This plugin is an example to turn `::note` into divs, passing arbitrary
+// attributes.
+/** @type {import('unified').Plugin<[], import('mdast').Root>} */
+function myRemarkPlugin () {
+  return (tree) => {
+    visit(tree, (node) => {
+      console.log(node);
+      if (
+        node.type === 'textDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'containerDirective'
+      ) {
+        if (node.name !== 'note') return;
+
+        const data = node.data || (node.data = {});
+        const attributes = node.attributes || {};
+        const tagName = node.type === 'textDirective' ? 'span' : 'div';
+
+        data.hName = tagName;
+        data.hProperties = h(tagName, attributes).properties;
+      }
+    });
+  };
+}
 
 const config = {
   extensions: ['.svelte.md', '.md', '.svx'],
   smartypants: {
     dashes: 'oldschool'
   },
-  remarkPlugins: [],
+  remarkPlugins: [
+    remarkParse,
+    remarkFrontmatter,
+    remarkDirective,
+    myRemarkPlugin,
+    remarkEmoji,
+    remarkGithub,
+    remarkGfm
+  ],
   rehypePlugins: [
-    [
-      rehypeAutolinkHeadings,
-      {
-        behaviour: 'wrap'
-      }
-    ]
+    [rehypeAutolinkHeadings, { behaviour: 'wrap' }]
   ]
 };
 
---
+++ src/routes/consume/2022-01-25-markdown-sample-small.md
@@ -0,0 +1,20 @@
+---
+title: "Markdown Sample Small"
+description: "A small sample markdown text with many attributes/tags etc."
+thumbnailUrl: "/favicon.ico"
+thumbnailTitle: "Icon representing markdown text documents"
+creationDate: "2022-01-25T03:21:13+01:00"
+authorName: "Michael Rommel"
+authorAvatarUrl: "https://avatars.githubusercontent.com/u/919935?s=40&v=4"
+tags: ["new", "locked", "consume", "code"]
+published: true
+---
+
+# h1 Heading
+
+:::note{.wide}
+Extension note
+Another paragraph line
+
+and the last one.
+:::
