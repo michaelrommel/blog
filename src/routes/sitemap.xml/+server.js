1944f9a4843bd8e4b602c7b25b01f09491540236
Michael Rommel <rommel@layer-7.net>
2024-09-01 12:50:42 +0200

    feat: add a sitemap.xml route for all articles

---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,29 @@
+export async function GET({ fetch, setHeaders }) {
+	let articleList = null;
+	articleList = await fetch(`/api/articles`).then((res) => res.json());
+	console.log(articleList);
+
+	setHeaders({
+		'Content-Type': 'application/xml'
+	});
+
+	let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset 
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+	xhtml="https://www.w3.org/1999/xhtml"
+	mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+	news="https://www.google.com/schemas/sitemap-news/0.9"
+	image="https://www.google.com/schemas/sitemap-image/1.1"
+	video="https://www.google.com/schemas/sitemap-video/1.1"
+>\n`;
+	for (const article of articleList) {
+		// <url>
+		// <loc>https://www.example.com/foo.html</loc>
+		// <lastmod>2022-06-04</lastmod>
+		//  </url>
+		sitemap += `<url>\n 	<loc>https://michaelrommel.com/${article.articleCategory}/${article.slug}</loc>\n 	<lastmod>${article.modificationDate ? article.modificationdate : article.creationDate}</lastmod>\n</url>\n`;
+	}
+	sitemap += '</urlset>';
+
+	return new Response(sitemap);
+}
