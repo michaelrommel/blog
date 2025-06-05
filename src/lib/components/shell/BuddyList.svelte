189555c50c1fc45bd7bc25aa175802c28fd1b9cd
Michael Rommel <rommel@layer-7.net>
2025-06-06 01:25:18 +0200

    fix: add eye icon for read-only buddies

--- src/lib/components/shell/BuddyList.svelte
+++ src/lib/components/shell/BuddyList.svelte
@@ -1,13 +1,14 @@
 <script>
 	import { nameToHue } from "./LiveCursor.svelte";
+	import { Eye } from "@lucide/svelte";
 
 	let { users } = $props();
 	let sortedUsers = $derived(
 		[...users].sort((a, b) => (b[1].name < a[1].name ? 1 : -1)),
 	);
-	// $effect(() => {
-	// 	console.log($state.snapshot(sortedUsers));
-	// });
+	$effect(() => {
+		console.log($state.snapshot(sortedUsers));
+	});
 </script>
 
 <div class="flex items-center p-1 bg-gruvdbg1/90 rounded-t-md">
@@ -19,16 +20,21 @@
 		<ul class="flex flex-col">
 			{#each sortedUsers as [id, user] (id)}
 				<li
-					class="flex p-1 gap-2 items-center"
+					class="flex p-1 gap-2 items-center overflow-hidden"
 					class:opacity-75={!user.canWrite}
 				>
 					<div
 						style:background="hsl({nameToHue(user.name)}, 100%, 50%)"
-						class="w-3.5 h-3.5 rounded-full border border-[#a89984]"
+						class="shrink-0 w-3.5 h-3.5 rounded-full border border-[#a89984]"
 					></div>
 
-					<div>
+					<div class="overflow-hidden max-w-24">
 						{user.name}
+					</div>
+					<div class="shrink-0 w-3.5">
+						{#if !user.canWrite}
+							<Eye size="18px" />
+						{/if}
 					</div>
 				</li>
 			{/each}
